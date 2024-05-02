ba00cddbdeb14b8c4ad9f429dac97e3f503ee1f0
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-05-02 12:01:59 +1200

    fst

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+#ask users name
+name=input("what is your name")
+#say hello to user and explain the quiz
+print("hello" ,name, "this is my quiz")
+print("this quiz is about math")
+#ask user a question
+input("what is 20 x 35")
+#say the answer
+print("the answer is 700 good job if you got it correct")
+#end
+print("this is the end thanks for playing")
