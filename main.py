d22badeefd69892c3d92b582991b1a26fc7b40aa
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-05-09 11:52:22 +1200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ name=input("what is your name").lower()
 #say hello to user and explain the quiz
 print("hello" ,name, "this is my quiz")
 print("this quiz is about math")
+print("dont put any spaces before your answers please")
 #ask user a question
 answer=input("what is 20 x 35").lower()
 #check the answer and tell user
@@ -22,8 +23,12 @@ answer = input("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).lower()
 if answer == a or answer == "a":
     print("nice you got it correct")
     score += 20
+elif answer == "":
+    print("that is not an answer")
+elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer !="d":
+    print("that is not an answer")
 else:
     print("haha u got it wrong")
 #end
-print("this is the end thanks for playing{}." .format(name))
+print("this is the end thanks for playing {}." .format(name))
 print("your final score is", score)
