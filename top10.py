e3aab39bf4ee07830ed1b5325f938058c8495df4
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-05-24 12:58:57 +1200

    Update top10.py

--- top10.py
+++ top10.py
@@ -2,7 +2,6 @@ import random
 
 score = 0
 MAX_TURNS = 10
-
 #---------functions----------
 
 #welcome to quiz
@@ -12,8 +11,36 @@ def intro():
 
     #hello user
     print("welcome to my quiz,",name)
-    print("this quiz is about the top ten numbers of all time have fun ;)")
+    print("this quiz is about the top ten  have fun ;)")\
+    
+def getpassword():
+    while True:
+        password = input("what is the password")
+        if password == "hehe":
+            return
+        else:
+            print("wrong answer try again")
+def getnumber():
+    number = input("give me number")
+    return int(number)
 
+number = getnumber()
+print(number)
+
+def getlives():
+    while True:
+        lives = input("how many attempts would you like")
+        try:
+            lives = int(lives)
+            if lives >= 0:
+                return lives
+            else:
+                print("a positive number please")
+        except:
+            print("that is not a number")
 #---------main code----------
 
 intro()
+lives = getlives()
+tries = getnumber()
+BN_ANSWERS = []
