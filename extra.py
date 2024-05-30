43b025eda34d880d9c863db874c6d1a809e7bbfd
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-05-30 12:30:22 +1200

    Update extra.py

--- extra.py
+++ extra.py
@@ -10,10 +10,10 @@ print("in my game we will both guess a number from 1-3 if the numbers are the sa
 while True:
     number = input("say a number from 1-3")
     if number == "1":
-        print("my number is 2")
+        print("my number is 2 i win hahaha")
     elif number == "2":
-        print("my number is 3")
+        print("my number is 3 i win hahaha")
     elif number == "3":
-        print("my number is 17")
+        print("my number is 17 i win hahaha")
     else:
         print("you cant do that")
